3611084f259d64d1f7ef1941045d4e92a7f41897
rohulaminwd <rohulaminwd@gmail.com>
2022-05-22 15:21:46 +0600

    user add and verify jwt add

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Hello world!! server is running')
 })
+// ======= middleware jwt ========
+function verifyJWT(req, res, next){
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({message: 'Unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
+    if(err){
+      return res.status(403).send({message: 'Forbidden access'})
+    } 
+    req.decoded = decoded;
+    next();
+  });
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwe6b.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,6 +38,44 @@ async function run(){
   try{
     await client.connect();
     const userCollection = client.db('manufacturer').collections('users')
+
+    //  ======= middleware verify Admin ========
+    const verifyAdmin = async (req, res, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({email: requester});
+      // console.log(requesterAccount);
+      if(requesterAccount.role === "admin"){
+        next();
+      }else{
+        return res.status(403).send({message: 'Forbidden access'})
+      }
+    }
+
+    // ====== put method ======
+    app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+        const filter = {email: email};
+        const updateDoc = {
+          $set: {role: "admin"}
+        };
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result)
+    })
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email: email};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({result, token})
+    })
+
+
   }finally{
 
   }
